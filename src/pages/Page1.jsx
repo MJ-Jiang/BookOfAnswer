ead952bf13cad870e21a717a1a818fbc47be983d
MJ-Jiang <maura_jiang@outlook.com>
2025-07-08 12:56:52 +0300

    page1

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -12,32 +12,36 @@ const Page1=() =>{
     //     return () => clearTimeout(timer); // Cleanup the timer on unmount
     // }, [navigate]);
     return (
-        <div className="page page1">
-            {/* Background of universe */}
+        <div className="page1">
             <div className="page1-bg"></div>
 
             <div className="grid-layout">
-                <div className="title-box pixel-font" >
+                <div className="title-box pixel-font">
                     <h1>答案之书</h1>
                     <h2>向杨而生，超越一切</h2>
                 </div>
-                <img 
-                src="/images/spaceship.png"
-                alt="spaceship"
-                className="spaceship"
-                onClick={()=> navigate("/page2")} // Navigate to page2 on click
-            />
 
+                <img
+                    src="/images/spaceship.png"
+                    alt="spaceship"
+                    className="spaceship"
+                    onClick={() => navigate("/page2")}
+                    />
+                </div>
+
+  {/* 星星在 grid-layout 外部，位置是自由布局 */}
+                <img src="/images/star1.png" alt="star1" className="star star1" />
+                <img src="/images/star2.png" alt="star2" className="star star2" />
+                <img src="/images/star3.png" alt="star3" className="star star3" />
+                <img src="/images/star4.png" alt="star4" className="star star4" />
+
+  {/* 模式按钮固定底部 */}
+                <div className="mode-buttons">
+                <div className="mode-button" onClick={() => navigate("/page4")}>对话版</div>
+                <div className="mode-button" onClick={() => navigate("/page2")}>答案之书版</div>
             </div>
-            
-
-            
-
-            {/* stars */}
-            <img src= "/images/star1.png" alt="star1" className="star star1" />
-            <img src= "/images/star2.png" alt="star2" className="star star2" />
-            <img src= "/images/star3.png" alt="star3" className="star star3" />
         </div>
+
     )
 
 }
