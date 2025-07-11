30d64ac789f7d6f3b8f4866758cbd2e96365ea4c
MJ-Jiang <maura_jiang@outlook.com>
2025-07-11 22:12:09 +0300

    before final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page1 from './pages/Page1';
-import Page2 from './pages/page2';
+import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
 import Page4 from './pages/Page4';
 import './App.css'; 
